fca561abe392866a5514635ed37643ff28718f16
joechesney <33613119+joechesney@users.noreply.github.com>
2018-08-20 17:39:59 -0500

    installed mysql and tried to set it up but it is not working yet

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express =require('express');
 
 const app = express();
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "admin",
+  password: "admin"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 app.use(express.static('public'));
 
