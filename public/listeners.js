2526ebde316c98db2c69d47d49f9b0aecd7fe3ee
joechesney <33613119+joechesney@users.noreply.github.com>
2018-08-21 14:45:36 -0500

    function attached to addTask button now grabs the pokestop id and sends it into the event object and i have access to it so i can use that in my post requests

--- public/listeners.js
+++ public/listeners.js
@@ -1,13 +1,16 @@
 import { addTask } from './addTask.js';
 export function addListeners() {
-  // document.getElementsByClassName("addTaskButton").addEventListener("click", e=>{
+  // window.addEventListener("click", e=>{
   //   e.preventDefault();
   //   console.log('add Task clicked! ',e);
   //   addTask(e);
   // })
   document.addEventListener("click", e => {
     e.preventDefault();
-
-    console.log('window clicked!', e);
+    console.log('window click! ',e);
+    console.log('window clicked!', e.target.id);
+    if (e.target.className === "addTaskButton"){
+      console.log('CHUPS',e.target.id);
+    }
   })
 }
