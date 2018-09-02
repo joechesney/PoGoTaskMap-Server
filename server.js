376ca4c6883494e9590766002e1825fca20d0817
joechesney <33613119+joechesney@users.noreply.github.com>
2018-09-02 13:33:46 -0500

    added env file back with port number and added inscureAuth back to the connection options object

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PW,
-  database: process.env.DB_DATABASE_NAME
+  database: process.env.DB_DATABASE_NAME,
+  insecureAuth: true
 });
 
 connection.connect((err) => {
