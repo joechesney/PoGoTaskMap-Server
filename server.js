d9341285d95e34407d95c3fbd81ec6b176fd255c
joechesney <33613119+joechesney@users.noreply.github.com>
2018-10-03 11:32:11 -0500

    added an app.use section that defines cors headers

--- server.js
+++ server.js
@@ -24,6 +24,12 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // This is used to keep the server alive
 setInterval(function () {
   connection.query('SELECT 1', function (err, result) {
