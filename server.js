b139d69f34c23645af74048e055707d4adf69855
joechesney <33613119+joechesney@users.noreply.github.com>
2018-10-06 16:15:24 -0500

    removed last change

--- server.js
+++ server.js
@@ -113,11 +113,6 @@ app.get('/getNewestPokestop/:pokestopId', (req, res, next) => {
 
   connection.query(`
   SELECT pokestops.*
-  CASE
-    WHEN tasks.task_date_end_time > NOW()
-    THEN 'true'
-    ELSE 'false'
-    END active
   FROM pokestops
   WHERE pokestops.id = ${req.params.pokestopId}
   `, (err, pokestop) => {
