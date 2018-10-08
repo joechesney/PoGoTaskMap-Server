1a7ef4ea2b23aba0856ffadb3bd56a3b39458dc8
joechesney <33613119+joechesney@users.noreply.github.com>
2018-10-08 12:39:46 -0500

    removed the forward slash from allowed url

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(function(req, res, next) {
   next();
 });
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://pogotaskmap.firebaseapp.com/");
+  res.header("Access-Control-Allow-Origin", "https://pogotaskmap.firebaseapp.com");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
