85c58eed97cc82d110ca1b6dfb48eb91c3a84a0e
joechesney <33613119+joechesney@users.noreply.github.com>
2018-10-08 12:38:01 -0500

    added methods to the cors use

--- server.js
+++ server.js
@@ -29,11 +29,13 @@ app.use(bodyParser.json());
 // CORS use route
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://pogotaskmap.firebaseapp.com/");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
